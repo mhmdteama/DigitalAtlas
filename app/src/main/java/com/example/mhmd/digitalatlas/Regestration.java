b4e8449b6f9016b132c9961847cefdc930bf3fae
MHMD <engteama@gmail.com>
2018-10-09 12:52:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mhmd/digitalatlas/Regestration.java
@@ -0,0 +1,15 @@
+package com.example.mhmd.digitalatlas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+
+public class Regestration extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regestration);
+
+    }
+}
