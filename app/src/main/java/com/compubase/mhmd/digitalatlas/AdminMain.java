a5114ff917a1c65307f5374195ed2a4d9ba6f581
MHMD <engteama@gmail.com>
2018-10-28 23:55:46 +0200

    update

--- app/src/main/java/com/compubase/mhmd/digitalatlas/AdminMain.java
+++ app/src/main/java/com/compubase/mhmd/digitalatlas/AdminMain.java
@@ -2,14 +2,18 @@ package com.compubase.mhmd.digitalatlas;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class AdminMain extends AppCompatActivity {
     ImageButton add, patientlisst, approve, notific;
-
+    TextView title;
+    Button logout , toimport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +23,36 @@ public class AdminMain extends AppCompatActivity {
         final AdminNotification adminNotification = new AdminNotification();
         final PaientList paientList = new PaientList();
         approve = findViewById(R.id.approve);
-        //final AddPatientFragment addPatientFragment = new AddPatientFragment();
-        //final PatientListAdmin patientListAdmin = new  PatientListAdmin();
-        // PatientNotification patientNotification = new PatientNotification();
-        // final NotificationAdmin notificationAdmin = new NotificationAdmin();
-        //FragmentManager fragmentManager = getFragmentManager();
-        // FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        //fragmentTransaction.add(R.id.cotainer , frag);
-        //fragmentTransaction.commit();
+        title = findViewById(R.id.titlmainadmin);
+        logout = findViewById(R.id.logout);
+        toimport = findViewById(R.id.toimportant);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(AdminMain.this , SignIn.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        toimport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(AdminMain.this , Document.class);
+                startActivity(intent);
+            }
+        });
+
+        title.setText("Add Finding(S)");
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.cotaineradmin,addFinfing1 );
+        fragmentTransaction.commit();
+
+
+
         add = findViewById(R.id.addfinfingadmin);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +61,9 @@ public class AdminMain extends AppCompatActivity {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.cotaineradmin,addFinfing1 );
                 fragmentTransaction.commit();
+
+                title.setText("Add Finding(S)");
+
             }
         });
         approve.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +73,10 @@ public class AdminMain extends AppCompatActivity {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.cotaineradmin,adminApproveList );
                 fragmentTransaction.commit();
+
+                title.setText("Approve Users");
+
+
             }
         });
         notific = findViewById(R.id.notificAdmin);
@@ -55,6 +88,8 @@ public class AdminMain extends AppCompatActivity {
                 fragmentTransaction.replace(R.id.cotaineradmin,adminNotification );
                 fragmentTransaction.commit();
 
+                title.setText("Notifications");
+
             }
         });
         patientlisst = findViewById(R.id.paierntlistadmin);
@@ -65,6 +100,9 @@ public class AdminMain extends AppCompatActivity {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.cotaineradmin,paientList );
                 fragmentTransaction.commit();
+
+
+                title.setText("Patient List");
 
             }
         });
--- app/src/main/java/com/compubase/mhmd/digitalatlas/MainActivity.java
+++ app/src/main/java/com/compubase/mhmd/digitalatlas/MainActivity.java
@@ -2,13 +2,21 @@ package com.compubase.mhmd.digitalatlas;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton add, patientlists, notific;
+    TextView title;
+    Button logout , toimport;
+
     final AddFinfing1 addFinfing1 = new AddFinfing1();
     final PaientList paientList = new PaientList();
     final UserNotification userNotification = new UserNotification();
@@ -17,6 +25,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+        title = findViewById(R.id.titlemainuser);
+        logout = findViewById(R.id.logout);
+        toimport = findViewById(R.id.toimportant);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this , SignIn.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        toimport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this , Document.class);
+                startActivity(intent);
+            }
+        });
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.usercontiner,addFinfing1 );
+        fragmentTransaction.commit();
+
+        title.setText("Add Finding(S)");
+
         add = findViewById(R.id.addfinfing);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.usercontiner,addFinfing1 );
                 fragmentTransaction.commit();
+
+                title.setText("Add Finding(S)");
+
             }
         });
         patientlists = findViewById(R.id.plistbutton);
@@ -36,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.replace(R.id.usercontiner,paientList );
                 fragmentTransaction.commit();
 
+                title.setText("Patient List");
+
             }
         });
         notific =findViewById(R.id.usernotific);
@@ -46,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.usercontiner,userNotification );
                 fragmentTransaction.commit();
+
+                title.setText("Notifications");
             }
         });
     }
